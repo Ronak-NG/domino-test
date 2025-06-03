1f962f0ce5054ba617e4fc7b8a0fe6ca226d845f
Ronak Patel <ronak@nationgraph.com>
2025-06-03 17:49:25 -0400

    fix asset statement

--- pieces/ImageFilterPiece/test_imagefilterpiece.py
+++ pieces/ImageFilterPiece/test_imagefilterpiece.py
@@ -25,6 +25,6 @@ def test_imagefilterpiece():
         piece_name="ImageFilterPiece",
         input_data=input_data
     )
-    for piece_output in piece_outputs:
+    for piece_output in piece_outputs.get('image_file_path'):
         assert piece_output is not None
-        assert piece_output.get('image_file_path').endswith('.png')
+        assert piece_output.endswith('.png')
