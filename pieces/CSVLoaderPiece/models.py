1ced69f3093a1ef2599c7902e318b3c20c7e2ea2
Ronak Patel <ronak@nationgraph.com>
2025-06-04 12:19:25 -0400

    updated model

--- pieces/CSVLoaderPiece/models.py
+++ pieces/CSVLoaderPiece/models.py
@@ -5,7 +5,14 @@ from enum import Enum
 
 class InputModel(BaseModel):
     input_csv_file: str = Field(
-        description='.csv file contents',
+        default="""
+url
+https://example.com/image1.jpg
+""",
+        description='CSV file contents - paste or edit your CSV data here',
+        json_schema_extra={
+            'widget': "codeeditor-text"
+        }
     )
 
 
